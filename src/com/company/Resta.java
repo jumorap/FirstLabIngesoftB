d7ca646bc33df2ee3931927bfbed03aeb2562b28
Proft1827 <srodriguezt@unal.edu.co>
2021-03-10 19:29:37 -0500

    First proof

---
+++ src/com/company/Resta.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Resta {
+
+    public Resta() {
+    }
+
+    public void printResta(double inputA, double inputB) {
+
+        double result = inputA - inputB;
+        System.out.print(result);
+    }
+
+}
